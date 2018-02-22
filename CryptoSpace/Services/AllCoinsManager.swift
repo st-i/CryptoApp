b29210ae6d092115bb443614ce4ac0bea832d3ed
Unknown <someone580@bk.ru>
2018-02-22 22:09:18 +0300

    30 of Top200 coins were added

--- CryptoSpace/Services/AllCoinsManager.swift
+++ CryptoSpace/Services/AllCoinsManager.swift
@@ -95,14 +95,146 @@ class AllCoinsManager: NSObject {
         allCoins.append(dashCoin)
         
         //11
+        let moneroCoin = Coin.init()
+        moneroCoin.fullName = "Monero"
+        moneroCoin.shortName = "XMR"
+        moneroCoin.id = "xmr"
+        allCoins.append(moneroCoin)
+        
+        //12
         let iotaCoin = Coin.init()
         iotaCoin.fullName = "IOTA"
         iotaCoin.shortName = "MIOTA"
         iotaCoin.id = "miota"
         allCoins.append(iotaCoin)
         
-        //12
+        //13
+        let nemCoin = Coin.init()
+        nemCoin.fullName = "NEM"
+        nemCoin.shortName = "XEM"
+        nemCoin.id = "xem"
+        allCoins.append(nemCoin)
         
+        //14
+        let ethereumClassicCoin = Coin.init()
+        ethereumClassicCoin.fullName = "Ethereum Classic"
+        ethereumClassicCoin.shortName = "ETC"
+        ethereumClassicCoin.id = "etc"
+        allCoins.append(ethereumClassicCoin)
+        
+        //15
+        let tronCoin = Coin.init()
+        tronCoin.fullName = "TRON"
+        tronCoin.shortName = "TRX"
+        tronCoin.id = "trx"
+        allCoins.append(tronCoin)
+        
+        //16
+        let veChainCoin = Coin.init()
+        veChainCoin.fullName = "VeChain"
+        veChainCoin.shortName = "VEN"
+        veChainCoin.id = "ven"
+        allCoins.append(veChainCoin)
+        
+        //17
+        let tetherCoin = Coin.init()
+        tetherCoin.fullName = "Tether"
+        tetherCoin.shortName = "USDT"
+        tetherCoin.id = "usdt"
+        allCoins.append(tetherCoin)
+        
+        //18
+        let liskCoin = Coin.init()
+        liskCoin.fullName = "Lisk"
+        liskCoin.shortName = "LSK"
+        liskCoin.id = "lsk"
+        allCoins.append(liskCoin)
+        
+        //19
+        let bitcoinGoldCoin = Coin.init()
+        bitcoinGoldCoin.fullName = "Bitcoin Gold"
+        bitcoinGoldCoin.shortName = "BTG"
+        bitcoinGoldCoin.id = "btg"
+        allCoins.append(bitcoinGoldCoin)
+        
+        //20
+        let qtumCoin = Coin.init()
+        qtumCoin.fullName = "Qtum"
+        qtumCoin.shortName = "QTUM"
+        qtumCoin.id = "qtum"
+        allCoins.append(qtumCoin)
+        
+        //21
+        let omiseGoCoin = Coin.init()
+        omiseGoCoin.fullName = "OmiseGo"
+        omiseGoCoin.shortName = "OMG"
+        omiseGoCoin.id = "omg"
+        allCoins.append(omiseGoCoin)
+        
+        //22
+        let iconCoin = Coin.init()
+        iconCoin.fullName = "ICON"
+        iconCoin.shortName = "ICX"
+        iconCoin.id = "icx"
+        allCoins.append(iconCoin)
+        
+        //23
+        let zcashCoin = Coin.init()
+        zcashCoin.fullName = "Zcash"
+        zcashCoin.shortName = "ZEC"
+        zcashCoin.id = "zec"
+        allCoins.append(zcashCoin)
+        
+        //24
+        let nanoCoin = Coin.init()
+        nanoCoin.fullName = "Nano"
+        nanoCoin.shortName = "NANO"
+        nanoCoin.id = "nano"
+        allCoins.append(nanoCoin)
+        
+        //25
+        let binanceCoin = Coin.init()
+        binanceCoin.fullName = "Binance Coin"
+        binanceCoin.shortName = "BNB"
+        binanceCoin.id = "bnb"
+        allCoins.append(binanceCoin)
+        
+        //26
+        let bytecoinCoin = Coin.init()
+        bytecoinCoin.fullName = "Bytecoin"
+        bytecoinCoin.shortName = "BCN"
+        bytecoinCoin.id = "bcn"
+        allCoins.append(bytecoinCoin)
+        
+        //27
+        let steemCoin = Coin.init()
+        steemCoin.fullName = "Steem"
+        steemCoin.shortName = "STEEM"
+        steemCoin.id = "steem"
+        allCoins.append(steemCoin)
+        
+        //28
+        let vergeCoin = Coin.init()
+        vergeCoin.fullName = "Verge"
+        vergeCoin.shortName = "XVG"
+        vergeCoin.id = "xvg"
+        allCoins.append(vergeCoin)
+        
+        //29
+        let populousCoin = Coin.init()
+        populousCoin.fullName = "Populous"
+        populousCoin.shortName = "PPT"
+        populousCoin.id = "ppt"
+        allCoins.append(populousCoin)
+        
+        //30
+        let stratisCoin = Coin.init()
+        stratisCoin.fullName = "Stratis"
+        stratisCoin.shortName = "STRAT"
+        stratisCoin.id = "strat"
+        allCoins.append(stratisCoin)
+        
+        //31 siacoin
         
         return allCoins
     }
